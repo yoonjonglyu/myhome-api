9721fd5a970dfefb10f6dea4ea0601791c47726e
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-12-14 17:17:21 +0900

    feat: API POST GET - TAGS, ESSAY, TECH, PORTPOLIO

--- api/post/index.js
+++ api/post/index.js
@@ -3,5 +3,17 @@ const router = require('express').Router();
 router.get('/', (req, res) => {
     res.send("Hello, POST");
 });
+router.get('/tags', (req, res) => {
+    res.send("Hello, Tags");
+});
+router.get('/essay', (req, res) => {
+    res.send("Hello, Essay");
+});
+router.get('/tech', (req, res) => {
+    res.send("Hello, Tech");
+});
+router.get('/portpolio', (req, res) => {
+    res.send("Hello, Portpolio");
+});
 
 module.exports = router;
