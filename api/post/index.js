0a2e4083c6bbc43f82bffaabf37e41f2a0a67506
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-12-18 23:03:56 +0900

    feat: API POST - GET TAGS TEMP MSG

--- api/post/index.js
+++ api/post/index.js
@@ -1,10 +1,20 @@
 const router = require('express').Router();
-
+const DB = require('../../database/index');
+const { portpolio_tags } = require('../../database/portpolio/portpolio');
 router.get('/', (req, res) => {
+
     res.send("Hello, POST");
 });
 router.get('/tags', (req, res) => {
-    res.send("Hello, Tags");
+    const TechTags = DB.queryTags("tech");
+    const EssayTags = DB.queryTags("essay");
+    const PortpolioTags = DB.queryTags("portpolio");
+    res.send(`
+    Hello, Tags<br />
+    Tech : ${TechTags}<br />
+    Essay : ${EssayTags}<br />
+    portpolio : ${PortpolioTags}<br />
+    `);
 });
 router.get('/essay', (req, res) => {
     res.send("Hello, Essay");
