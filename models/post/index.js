8b2e12f858b392ba13e181da40f35f19b606e2a3
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-12-19 14:48:51 +0900

    feat: POST MODELS - GET TAGS

--- models/post/index.js
+++ models/post/index.js
@@ -1,8 +1,18 @@
 /**
  * Post models
  */
-class PostModels {
+const DAO = require('../dao');
 
+class PostModels {
+    getTags() {
+        const result = {
+            essay: DAO.getEssayTags(),
+            tech: DAO.getTechTags(),
+            portpolio: DAO.getPortpolioTags()
+        }
+
+        return result;
+    }
 }
 
-module.exports = new PostModels
+module.exports = new PostModels();
