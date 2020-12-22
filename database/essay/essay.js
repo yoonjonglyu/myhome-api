6546f3628bb56cace68e5cdf261d6144f9d83e59
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-12-22 22:37:17 +0900

    feat: DATABASE -  BASIC POST STRUCTURE

--- database/essay/essay.js
+++ database/essay/essay.js
@@ -1,8 +1,10 @@
+
 const essay = {
     essay_tags : [],
     essay_lists : [
+        require('./esy_1'),
 
     ],
 };
-
+console.log(essay);
 module.exports = essay;
---
+++ database/essay/esy_1.js
@@ -0,0 +1,12 @@
+const Essay = {
+    title : "도",
+    author : "ISA",
+    tags : ["도"],
+    description : "동양 철학에서 말하는 도 라는 것에 대한 개인적인 에세이",
+    contents : `
+        <h3>도</h3>
+        임시 글 내용
+    `    
+};
+
+module.exports = Essay;
