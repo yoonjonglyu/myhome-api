ae00c488ba0d5d60bb14a7f54ce131302268f148
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-12-27 13:34:55 +0900

    feat: CONFIG - TABLE NAME

--- config/index.js
+++ config/index.js
@@ -5,6 +5,11 @@ dotenv.config();
 const config = {
     PORT : process.env.PORT || 8080,
     DATABASE_HOST : process.env.DATABASE_URI,
-}
+    TABLE : {
+        ESSAY : "essay",
+        TECH : "tech",
+        PORTFOLIO : "portpolio"
+    },
+};
 
 module.exports = config;
