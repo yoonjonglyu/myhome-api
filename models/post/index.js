d5718777a1119ef6a7710460fdbde3f1b1778c54
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-12-27 14:11:21 +0900

    feat: MODELS POST MODELS - GET ESSAY & TECH & PORTPOLIO LIST

--- models/post/index.js
+++ models/post/index.js
@@ -4,12 +4,33 @@
 const DAO = require('../dao');
 
 class PostModels {
-    getTags() {
+    getTags () {
         const result = {
             essay: DAO.getEssayTags(),
             tech: DAO.getTechTags(),
             portpolio: DAO.getPortpolioTags()
         }
+
+        return result;
+    }
+    getEssayList () {
+        const result = {
+            essay : DAO.getEssayList(),
+        };
+
+        return result;
+    }
+    getTechList () {
+        const result = {
+            tech : DAO.getTechList(),
+        };
+
+        return result;
+    }
+    getPortpolioList () {
+        const result = {
+            portpolio : DAO.getPortpolioList(),
+        };
 
         return result;
     }
