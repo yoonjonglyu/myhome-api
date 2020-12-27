61b8d58ae0ca5377ae70840fddffe06249103b7e
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-12-27 14:14:42 +0900

    feat: API POST - ESSAY & TECH & PORTPOLIO RESPONSE

--- api/post/index.js
+++ api/post/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { TagsController } = require('../../controller');
+const { TagsController, PostController } = require('../../controller');
 
 router.get('/', (req, res) => {
 
@@ -8,20 +8,46 @@ router.get('/', (req, res) => {
 router.get('/tags', (req, res) => {
     try {
         const tags = TagsController.getTags();
+
         res.status(200).json(tags);
-    } catch (e){
+    } catch (e) {
         res.status(400).send("tags를 불러오는 과정에서 예기치 못한 장애가 발생했습니다.");
+
         console.error(e);
     }
 });
 router.get('/essay', (req, res) => {
-    res.send("Hello, Essay");
+    try {
+        const EssayList = PostController.getEssayList();
+
+        res.status(200).json(EssayList);
+    } catch (e) {
+        res.status(400).send("에세이 목록을 불러오는 과정에서 예기치 못한 장애가 발생했습니다.");
+
+        console.error(e);
+    }
 });
 router.get('/tech', (req, res) => {
-    res.send("Hello, Tech");
+    try {
+        const TechList = PostController.getTechList();
+
+        res.status(200).json(TechList);
+    } catch (e) {
+        res.status(400).send("기술 목록을 불러오는 과정에서 예기치 못한 장애가 발생했습니다.");
+
+        console.error(e);
+    }
 });
 router.get('/portpolio', (req, res) => {
-    res.send("Hello, Portpolio");
+    try {
+        const PortpolioList = PostController.getPortpolioList();
+
+        res.status(200).json(PortpolioList);
+    } catch (e) {
+        res.status(400).send("포트폴리오 목록을 불러오는 과정에서 예기치 못한 장애가 발생했습니다.");
+
+        console.error(e);
+    }
 });
 
 module.exports = router;
