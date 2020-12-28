b55d3957ecda03a602b070c5f841e82d8af2cad9
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-12-28 22:38:14 +0900

    feat: DATABASE - ESSAY 1 TEMP DATE

--- database/essay/esy_1.js
+++ database/essay/esy_1.js
@@ -5,6 +5,7 @@ const Essay = {
     title : "도",
     author : "ISA",
     tags : [insertTag(ESSAY, 1, "도")],
+    date : "2020-12-06",
     description : "동양 철학에서 말하는 도 라는 것에 대한 개인적인 에세이",
     contents : `
         <h3>도</h3>
