af4f67201ca24b87497e5565e6ae64c7cc15b6d0
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-12-28 22:45:51 +0900

    fix: SERVICE POST - GET LIST DATA FORMAT

--- service/post/index.js
+++ service/post/index.js
@@ -4,16 +4,46 @@ const { PostModels } = require('../../models');
 class PostService {
     getEssayList () {
         const result = PostModels.getEssayList();
+        result.essay = result.essay.map((post) => {
+            return {
+                idx : post.idx,
+                title : post.title,
+                author : post.author,
+                tags : post.tags,
+                date : post.date,
+                description : post.description,
+            };
+        })
 
         return result;
     }
     getTechList () {
         const result = PostModels.getTechList();
+        result.tech = result.tech.map((post) => {
+            return {
+                idx : post.idx,
+                title : post.title,
+                author : post.author,
+                tags : post.tags,
+                date : post.date,
+                description : post.description,
+            };
+        })
 
         return result;
     }
     getPortpolioList () {
         const result = PostModels.getPortpolioList();
+        result.portpolio = result.portpolio.map((post) => {
+            return {
+                idx : post.idx,
+                title : post.title,
+                author : post.author,
+                tags : post.tags,
+                date : post.date,
+                description : post.description,
+            };
+        })
 
         return result;
     }
