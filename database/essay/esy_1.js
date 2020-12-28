98832f1d039461328611924b664e762d97973cd7
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-12-28 23:28:12 +0900

    fix: DATABASE ESSAY - ESSAY 1 TEMP CONTENTS

--- database/essay/esy_1.js
+++ database/essay/esy_1.js
@@ -7,10 +7,8 @@ const Essay = {
     tags : [insertTag(ESSAY, 1, "도")],
     date : "2020-12-06",
     description : "동양 철학에서 말하는 도 라는 것에 대한 개인적인 에세이",
-    contents : `
-        <h3>도</h3>
-        임시 글 내용
-    `    
+    contents : 
+    `<h3>도</h3><br />임시 글 내용`    
 };
 
 module.exports = insertPost(ESSAY, 1, Essay);
