c97e5faccfcffaf8655be6acbdffaccf3123ca57
Isa(yunjongryu) <yunjonglyu@gmail.com>
2020-12-28 23:27:24 +0900

    fix: DATABASE DB - QUERY POST RESULT NULL HANDLE

--- database/index.js
+++ database/index.js
@@ -35,10 +35,11 @@ class DB {
         return result;
     }
     queryPost (table, idx) {
-        const result = Array.from(DATA[table.toUpperCase()][`${table}_lists`][idx]);
+        const result = Array.from(DATA[table.toUpperCase()][`${table}_lists`])[idx];
         
-        if(result.length === 0){
+        if(result === undefined){
             console.log(`${table}의 검색 결과를 찾을 수 없습니다.`);
+            return [];
         } else if (result === undefined){
             console.error(`${table} tag의 질의과정에서 문제가 발생 되었습니다.`)
             return [];
